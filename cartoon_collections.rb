8b6c81a02865d441f4642bf02092f0e60fa8665f
Tonny Oseko <tonnyoseko0@gmail.com>
2022-08-21 21:51:41 +0300

    done

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,23 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)
   # Your code here
+  dwarves.each_with_index do |dwarf, index|
+    puts "#{index.to_i + 1}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(calls)
+  calls.collect { |call| call.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(calls)
   # Your code here
+    calls.length > 4 ? false : true 
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(cheese)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese.detect do |cheese|
+    cheese.include?("cheddar")
+  end
 end
